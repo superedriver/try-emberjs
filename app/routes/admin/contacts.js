29697574b913e8c2f4f629abe0536393faeed0fb
super edriver <superedriver@gmail.com>
2016-05-05 21:07:18 +0300

    added delete buttons

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -3,25 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('contact');
+  },
+
+  actions: {
+
+    deleteContact(contact) {
+      let confirmation = confirm('Are you sure?');
+
+      if (confirmation) {
+        contact.destroyRecord();
+      }
+    }
   }
-  //,
-  //actions: {
-  //  saveMessage() {
-  //
-  //    const email = this.get('emailAddress');
-  //    const message = this.get('message');
-  //
-  //    const newContact = this.store.createRecord('contact', {
-  //      email: email,
-  //      message: message
-  //    });
-  //
-  //    newContact.save().then(() => {
-  //      alert(`We have sent the message on your e-mail: ${this.get('emailAddress')}`);
-  //      this.set('responseMessage', "We got your message and we'll get in touch soon");
-  //      this.set('emailAddress', '');
-  //      this.set('message', '');
-  //    });
-  //  }
-  //}
 });
