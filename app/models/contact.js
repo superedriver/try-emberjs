5e28367e66c1af27d749484158cfb70dfbdc6781
super edriver <superedriver@gmail.com>
2016-05-06 20:16:46 +0300

    before 7

--- app/models/contact.js
+++ app/models/contact.js
@@ -4,12 +4,4 @@ import attr from 'ember-data/attr';
 export default Model.extend({
   email: attr('string'),
   message: attr('string')
-  //,
-  //
-  //emailAddress: '',
-  //
-  //isValidEmail: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-  //isValidMessage: Ember.computed.gte('message.length', 5),
-  //isValidAll: Ember.computed.and('isValidEmail', 'isValidMessage'),
-  //isDisabled: Ember.computed.not('isValidAll')
 });
