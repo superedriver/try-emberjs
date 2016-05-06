d8dd9b5f45f0de9c35e7418401c0dee76ed9a4ed
super edriver <superedriver@gmail.com>
2016-05-06 19:22:56 +0300

    before 6

--- app/models/library.js
+++ app/models/library.js
@@ -1,8 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import Ember from 'ember';
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
-  phone: attr('string')
+  phone: attr('string'),
+
+  isValid: Ember.computed.notEmpty('name')
 });
